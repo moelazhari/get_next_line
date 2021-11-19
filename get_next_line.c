49402b7ad2cca924e018a47c7f4affe8e6c86f19
mazhari <mazhari@student.1337.ma>
2021-11-19 01:04:09 +0100

    some progress

--- get_next_line.c
+++ get_next_line.c
@@ -3,109 +3,120 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mazhari <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 12:57:13 by mazhari           #+#    #+#             */
-/*   Updated: 2021/11/16 16:44:50 by mazhari          ###   ########.fr       */
+/*   Updated: 2021/11/19 01:00:18 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
 
-int	fill_str(int fd, char **str)
-{	
-	char *buf;
-	char *forfree;
-	int	bayts;
 
-	buf = (char *)malloc(BUFFER_SIZE + 1 * sizeof(char));
+char *fill_str(int fd, char *str)
+{
+	char *buf;
+	int bayts;
+	
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)
-		return (-1);
-	if(read(fd, buf, 0) < 0)
-	{
-		free(buf);
-		return (-1);
-	}
+		return (NULL);
 	bayts = 1;
-	while (!ft_strchr(*str, '\n') && bayts != 0)
+	while (!ft_strchr(str, '\n') && bayts != 0)
 	{
-		bayts =	 read(fd, buf, BUFFER_SIZE);
-		if(bayts < 0)
+		bayts = read(fd, buf, BUFFER_SIZE);
+		if (bayts == -1)
 		{
 			free(buf);
-			return (-1);
+			return (NULL);
 		}
-		forfree = *str;
 		buf[bayts] = '\0';
-		*str = ft_strjoin(*str, buf);
-		free(forfree);
+		str = ft_strjoin(str, buf);
 	}
-	return (bayts);
+	free(buf);
+	return (str);
 }
 
-void fill_line(char *str, char **line)
+char *fill_line(char *str)
 {
-	char *forfree;
+	char *line;
 	int i;
 
 	i = 0;
-	forfree = str;
-	if (ft_strchr(str, '\n'))
+	if (!str)
+		return (NULL);
+	while (str[i] != '\n' && str[i])
+		i++;
+	line = (char *)malloc((i + 2) * sizeof(char));
+	if(!line)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\n' && str[i])
 	{
-		while (str[i] != '\n' && str[i])
-			i++;
-		*line = ft_substr(str, 0, i + 1);
-		str = ft_strdup(str + i + 1);
-		free(forfree);
+		line[i] = str[i];
+		i++;
 	}
-	else
+	if (str[i] == '\n')
 	{
-		*line = ft_strdup(str);
-		free(str);
-		str = NULL;
+		line[i] = str[i];
+		i++;
 	}
+	line[i] = '\0';
+	return (line);
 }
 
-char	*get_next_line(int fd)
+char *fill_new_str(char *str)
 {
-	char	*line;
- 	static char	*str;
-	int	bayts;
+	int i;
+	int j;
+	char *new_str;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (NULL);
+	}
+	new_str = (char *)malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!new_str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (str[i] != '\n' && str[i])
+		new_str[j++] = str[i++];
+	new_str[j] = '\0';
+	free(str);
+	return (new_str);
+}
+char *get_next_line(int fd)
+{
+	char *line;
+	static char *str;
 	
 	str = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);	
-	if (str == NULL)
-		str = ft_strdup("");
+		return (NULL);
 	if(!str)
+		str = ft_strdup("");
+	str = fill_str(fd, str);
+	if (!str)
 		return (NULL);
-	bayts = fill_str(fd, &str);
-	if(bayts < 0)
-	{
-		free(str);
-		return (NULL);
-	}
-	fill_line(str, &line);
-	if (bayts == 0 && line[0] == '\0')
-		return (0);
+	line = fill_line(str);
+	str = fill_new_str(str);
 	return (line);
 }
-/*
+
 int main()
 {
-    int        fd;
-    char    *line;
+	int fd;
+	char *line;
 
-    fd = open("klist.txt", O_RDWR);
-    if (fd < 0)
-        return (printf("ERR"));
-	
-	while((line = get_next_line(fd)))
-	{
-    	printf("%s\n", line);
-		printf("\n");
-	}
-    return (0);
+	fd = open("klist.txt", O_RDWR);
+	if (fd < 0)
+		return (printf("ERR"));
+	while (line = get_next_line(fd))
+		printf("%s", line);
+	return (0);
 }
-*/
--- get_next_line.h
+++ get_next_line.h
@@ -4,16 +4,11 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 1000
+#define BUFFER_SIZE 42
 
-
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	ft_bzero( void *s, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *left_str, char *buff);
 char	*ft_strchr(const char *s, int c);
 char   	*get_next_line(int fd);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+
